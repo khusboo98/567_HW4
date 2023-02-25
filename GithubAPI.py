64991bb99647f3a278d90e5eb6a8c299dd09a8e5
Khusboo Patel <47909633+khusboo98@users.noreply.github.com>
2023-02-24 20:34:51 -0500

    Add files via upload

---
+++ GithubAPI.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 23 21:41:07 2023
+
+@author: khusboopatel
+"""
+
+import json
+import pprint
+import os
+import requests
+
+# userInput = input("Username: ")
+userInput = "khusboo98"
+
+def gitHubFunction(userInput):
+    if (userInput == "" or userInput == []):
+        return "You must provide a username"
+    elif (isinstance(userInput, str) != True):
+        return "The input username is not valid"
+    elif((requests.get("https://api.github.com/users/" + userInput + "/repos")).json() == []):
+        return "This account does not have any repositories"
+    else:
+        repoName = requests.get("https://api.github.com/users/" + userInput + "/repos")
+        data = repoName.json()
+        try:
+            for i in data:
+                name = i["name"]
+                commits = requests.get("https://api.github.com/repos/" + userInput + "/" + name + "/commits")
+                commits_data = len(commits.json())
+                # commits_data = commits.json()
+                print("Repo:" + name + " Number of commits: " + str(commits_data))
+        except:
+            print("================================================================")
+            print("     YOU HAVE EXCEEDED YOUR API RATE LIMIT. SEE BELOW")
+            print("================================================================")
+            print(os.system("curl -i https://api.github.com/users/" + userInput))
+
---
+++ TestGithubAPI.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 23 21:40:57 2023
+
+@author: khusboopatel
+"""
+
+import unittest
+
+from GithubAPI import gitHubFunction
+
+class TestGitHubFunction(unittest.TestCase):
+    def testValidUserInput1(self):
+        self.assertEqual(gitHubFunction(1), 'The input username is not valid')
+
+    def testValidUserInput2(self):
+        self.assertEqual(gitHubFunction(10), 'The input username is not valid')
+        self.assertEqual(gitHubFunction(3012), 'The input username is not valid')
+        self.assertEqual(gitHubFunction([1,2,3]), 'The input username is not valid')
+    
+    def testValidUserInput3(self):
+        self.assertEqual(gitHubFunction("asdasdas"), 'This account does not have any repositories')
+        self.assertFalse(gitHubFunction("/.123/.123"), 'This account does not have any repositories')
+
+    def testValidUserInput4(self):
+        self.assertEqual(gitHubFunction(""), 'You must provide a username')
+        self.assertEqual(gitHubFunction([]), 'You must provide a username')
+
+if __name__ == '__main__':
+    print('Running Unit Tests')
+    unittest.main()
