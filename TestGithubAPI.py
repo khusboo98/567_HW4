b22676c730936dca95266c60120e4ad977401e91
Khusboo Patel <47909633+khusboo98@users.noreply.github.com>
2023-03-21 22:22:38 -0400

    Update TestGithubAPI.py

--- TestGithubAPI.py
+++ TestGithubAPI.py
@@ -6,27 +6,64 @@ Created on Thu Feb 23 21:40:57 2023
 @author: khusboopatel
 """
 
+import requests
 import unittest
-
+from unittest.mock import patch, MagicMock
 from GithubAPI import gitHubFunction
 
 class TestGitHubFunction(unittest.TestCase):
-    def testValidUserInput1(self):
-        self.assertEqual(gitHubFunction(1), 'The input username is not valid')
 
-    def testValidUserInput2(self):
-        self.assertEqual(gitHubFunction(10), 'The input username is not valid')
-        self.assertEqual(gitHubFunction(3012), 'The input username is not valid')
-        self.assertEqual(gitHubFunction([1,2,3]), 'The input username is not valid')
-    
-    def testValidUserInput3(self):
-        #self.assertEqual(gitHubFunction("asdasdas"), 'This account does not have any repositories')
-        self.assertFalse(gitHubFunction("/.123/.123"), 'This account does not have any repositories')
+    # Test that an empty or None input returns an error message
+    def test_empty_input(self):
+        result = gitHubFunction("")
+        self.assertEqual(result, "You must provide a username")
 
-    def testValidUserInput4(self):
-        self.assertEqual(gitHubFunction(""), 'You must provide a username')
-        self.assertEqual(gitHubFunction([]), 'You must provide a username')
+        result = gitHubFunction([])
+        self.assertEqual(result, "You must provide a username")
 
-if __name__ == '__main__':
-    print('Running Unit Tests')
-    unittest.main()
+    # Test that a non-string input returns an error message
+    def test_invalid_input(self):
+        result = gitHubFunction(123)
+        self.assertEqual(result, "The input username is not valid")
+
+        result = gitHubFunction({"username": "test"})
+        self.assertEqual(result, "The input username is not valid")
+
+    # Test that an account with no repositories returns an error message
+    @patch('requests.get')
+    def test_no_repositories(self, mock_get):
+        mock_response = MagicMock()
+        mock_response.json.return_value = []
+        mock_get.return_value = mock_response
+
+        result = gitHubFunction("testuser")
+        self.assertEqual(result, "This account does not have any repositories")
+
+    # Test that the function returns the expected result for a valid input
+    @patch('requests.get')
+    def test_valid_input(self, mock_get):
+        mock_response1 = MagicMock()
+        mock_response1.json.return_value = [{"name": "repo1"}, {"name": "repo2"}]
+        mock_get.side_effect = [mock_response1, mock_response1]
+
+        mock_response2 = MagicMock()
+        mock_response2.json.return_value = [{"sha": "abc123"}, {"sha": "def456"}]
+        mock_get.return_value = mock_response2
+
+        result = gitHubFunction("testuser")
+        expected_result = "Repo:repo1 Number of commits: 2\nRepo:repo2 Number of commits: 2\n"
+        self.assertEqual(result, expected_result)
+
+    # Test that the function returns an error message when the API rate limit is exceeded
+    @patch('os.system')
+    @patch('requests.get')
+    def test_api_rate_limit(self, mock_get, mock_os):
+        mock_response = MagicMock()
+        mock_response.raise_for_status.side_effect = requests.exceptions.HTTPError()
+        mock_get.return_value = mock_response
+
+        mock_os.return_value = ""
+
+        result = gitHubFunction("testuser")
+        self.assertTrue("YOU HAVE EXCEEDED YOUR API RATE LIMIT" in result)
+
